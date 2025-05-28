68ab368be27408fb4bb1c17e668591e85d698cc0
CodeMaster <codingexpert483@gmail.com>
2025-05-28 18:28:27 +0530

    Bug Fix

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -3,25 +3,32 @@ import "./gallery.css";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import InfiniteScroll from "react-infinite-scroll-component";
 import axios from "axios";
+import Skeleton from "../skeleton/skeleton";
 
-const fetchPins = async ({ pageParam, search, userId,boardId}) => {
+const fetchPins = async ({ pageParam, search, userId, boardId }) => {
   const res = await axios.get(
-    `${import.meta.env.VITE_API_ENDPOINT}/pins?cursor=${pageParam}&search=${search || ""}&userId=${userId || ""}&boardId=${boardId || ""}`,
+    `${import.meta.env.VITE_API_ENDPOINT}/pins?cursor=${pageParam}&search=${
+      search || ""
+    }&userId=${userId || ""}&boardId=${boardId || ""}`
   );
   return res.data;
 };
-const Gallery = ({ search , userId , boardId}) => {
+
+const Gallery = ({ search, userId, boardId }) => {
   const { data, fetchNextPage, hasNextPage, status } = useInfiniteQuery({
-    queryKey: ["pins" ,search],
-    queryFn: ({pageParam=0})=>fetchPins({pageParam, search, userId, boardId}),
+    // queryKey: ["pins"],
+    // FIXED QUERY KEY
+    queryKey: ["pins", search, userId, boardId],
+    queryFn: ({ pageParam = 0 }) =>
+      fetchPins({ pageParam, search, userId, boardId }),
     initialPageParam: 0,
     getNextPageParam: (lastPage, pages) => lastPage.nextCursor,
   });
 
-  if (status === "pending") return <div>Loading...</div>;
-  if (status === "error") return <div>Something went wrong</div>;
-
-  console.log(data);
+  // FIXED: ADD SKELETON LOADING
+  // if (status === "pending") return "Loading...";
+  if (status === "pending") return <Skeleton/>;
+  if (status === "error") return "Something went wrong...";
 
   const allPins = data?.pages.flatMap((page) => page.pins) || [];
 
@@ -30,12 +37,8 @@ const Gallery = ({ search , userId , boardId}) => {
       dataLength={allPins.length}
       next={fetchNextPage}
       hasMore={!!hasNextPage}
-      loader={<h4 style={{ textAlign: "center" }}>Loading...</h4>}
-      endMessage={
-        <p style={{ textAlign: "center" }}>
-          <b>You have seen it all</b>
-        </p>
-      }
+      loader={<h4>Loading more pins</h4>}
+      endMessage={<h3>All Posts Loaded!</h3>}
     >
       <div className="gallery">
         {allPins?.map((item) => (
--- src/components/image/image.jsx
+++ src/components/image/image.jsx
@@ -1,23 +1,23 @@
-import { IKImage } from 'imagekitio-react'
+import { IKImage } from "imagekitio-react";
 
-const Image = ({path,src,classname,alt,w,h}) => {
+const Image = ({ path, src, alt, className, w, h }) => {
   return (
     <IKImage
-        urlEndpoint={import.meta.env.VITE_URL_IK_ENDPOINT}
-        path={path}
-        src={src}
-        transformation={[
-          {
-            height: h,
-            width: w,
-          },
-        ]}
-        alt={alt}
-        className={classname}
-        loading="lazy"
-        lqip={{ active: true , quality: 20 }}
-      />
-  )
-}
+      urlEndpoint={import.meta.env.VITE_URL_IK_ENDPOINT}
+      path={path}
+      src={src}
+      transformation={[
+        {
+          height: h,
+          width: w,
+        },
+      ]}
+      alt={alt}
+      loading="lazy"
+      className={className}
+      lqip={{ active: true, quality: 20 }}
+    />
+  );
+};
 
-export default Image
+export default Image;
